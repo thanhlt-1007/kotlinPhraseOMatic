fb63c9360300ecd67b2fe1c6a8b2cf6d6abaab8f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2023-08-09 15:27:59 +0700

    Implement main's logic

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,21 @@
 fun main() {
-  println("Hello main")
+  val WORDS_1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic", "pervasive")
+  val WORDS_2 = arrayOf("empowered", "leveraged", "aligned", "targeted")
+  val WORDS_3 = arrayOf("process", "paradigm", "solution", "portal", "vision")
+
+  val SIZE_1 = WORDS_1.size
+  val SIZE_2 = WORDS_2.size
+  val SIZE_3 = WORDS_3.size
+
+  val INDEX_1 = (Math.random() * SIZE_1).toInt()
+  val INDEX_2 = (Math.random() * SIZE_2).toInt()
+  val INDEX_3 = (Math.random() * SIZE_3).toInt()
+
+  val WORD_1 = WORDS_1[INDEX_1]
+  val WORD_2 = WORDS_2[INDEX_2]
+  val WORD_3 = WORDS_3[INDEX_3]
+
+
+  val PHRASE = "${WORD_1} ${WORD_2} ${WORD_3}"
+  println(PHRASE)
 }
